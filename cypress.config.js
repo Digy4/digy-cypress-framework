bda378ef5702af03eb45c6902220417c140d57fe
Lander Joshua Vitug <lander.vitug@gmail.com>
2022-11-14 10:27:53 -0500

    add copyright message to some files

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,8 @@
+// Copyright (c) 2022 Digy4 Inc. and its affiliates. All rights reserved.
+// Unauthorized copying of this file, via any medium is strictly prohibited
+// Proprietary and confidential
+// Any illegal or unauthorized usage or violations will result in immediate legal action.
+
 const { createEsbuildPlugin } = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-preprocessor");
