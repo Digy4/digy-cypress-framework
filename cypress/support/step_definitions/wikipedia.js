ab599a47c5a8c9fe6d65b8e3314745d7b03f303f
Digy4 Tech <digy4tech@digy4.com>
2023-09-04 15:42:21 +0100

    Upgrade framework to Cypress 13.1.0

--- cypress/support/step_definitions/wikipedia.js
+++ cypress/support/step_definitions/wikipedia.js
@@ -2,7 +2,7 @@ import {Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 const linkCount = 5 // change back to 40 for 9 minute runtime
 
-const clickAllLinks = async () => {
+const clickAllLinks = () => {
   for (let i = 0; i < linkCount; i++) {
     cy.get(`p>a`).eq(i).click({force:true});
     cy.wait(5000);
